0d13a92103106433f66ff508a4fd80c0830118bc
OSTRY <adrian.mateusz.ostrowski@gmail.com>
2019-08-28 19:02:32 +0200

    Admin Pages cz.11c (konfiguracja menagera plikow)

--- CmsShop/Scripts/ckeditor/config.js
+++ CmsShop/Scripts/ckeditor/config.js
@@ -8,4 +8,10 @@ CKEDITOR.editorConfig = function( config )
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+
+    var roxyFileman = '/fileman/index.html?integration=ckeditor';
+    config.filebrowserBrowseUlr = roxyFileman;
+    config.filebrowserImageBrowseUlr = roxyFileman + '&type=image';
+    config.removeDialogTabs = 'link:upload;image:upload';
+
 };
