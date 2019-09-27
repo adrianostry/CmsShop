f4670615980a976bc05a546031689e9fdbf8c639
OSTRY <adrian.mateusz.ostrowski@gmail.com>
2019-09-27 18:37:34 +0200

    Admin Shop cz.20 (Galeria Zdjęć)

--- CmsShop/Areas/Admin/Controllers/ShopController.cs
+++ CmsShop/Areas/Admin/Controllers/ShopController.cs
@@ -477,5 +477,40 @@ namespace CmsShop.Areas.Admin.Controllers
 
             return RedirectToAction("Products");
         }
+
+        // POST: Admin/Shop/SaveGalleryImages
+        [HttpPost]
+        public ActionResult SaveGalleryImages(int id)
+        {
+            // petla po obrazkach 
+            foreach (string fileName in Request.Files)
+            {
+                // inicjalizacja 
+                HttpPostedFileBase file = Request.Files[fileName];
+
+                // Sprawdzanie czy jest plik i czy nie jest pusty
+                if (file != null && file.ContentLength > 0)
+                {
+                    // ustawienie sciezki do katalogu 
+                    var originalDirectory = new DirectoryInfo(string.Format("{0}Images\\Uploads", Server.MapPath(@"\")));
+
+                    string pathString1 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString() + "\\Gallery");
+                    string pathString2 = Path.Combine(originalDirectory.ToString(), "Products\\" + id.ToString() + "\\Gallery\\Thumbs");
+
+                    var path = string.Format("{0}\\{1}", pathString1, file.FileName);
+                    var path2 = string.Format("{0}\\{1}", pathString2, file.FileName);
+
+                    // zapis miniaturek i obrazkow
+                    file.SaveAs(path);
+                    WebImage img = new WebImage(file.InputStream);
+                    img.Resize(200, 200);
+                    img.Save(path2);
+
+                }
+            }
+
+
+            return View();
+        }
     }
 }
