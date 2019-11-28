b392ebe2a5b92e444f39f09fdb26cac6b7c8871c
OSTRY <adrian.mateusz.ostrowski@gmail.com>
2019-11-28 19:08:01 +0100

    Rehestracja/Logowanie Użytkowników cz.1 (dodanie tabelek)

---
+++ CmsShop/Models/Data/UserDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CmsShop.Models.Data
+{
+    [Table("tblUsers")]
+    public class UserDTO
+    {
+        [Key]
+        public int Id { get; set; }
+        public string FirsName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddres { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
